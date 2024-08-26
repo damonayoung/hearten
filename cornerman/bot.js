8de3707b00398b8a7d1ec036834d00842a293c32
Damon Young <dayoung@gmail.com>
2024-08-25 19:45:58 -0700

    add logging from Claude to bot.js

--- cornerman/bot.js
+++ cornerman/bot.js
@@ -1,28 +1,37 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
-class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+   class EchoBot extends ActivityHandler {
+       constructor() {
+           super();
+           this.onMessage(async (context, next) => {
+               console.log(`Received message: ${context.activity.text}`);
+               const replyText = `Echo: ${context.activity.text}`;
+               console.log(`Sending reply: ${replyText}`);
+               await context.sendActivity(MessageFactory.text(replyText, replyText));
+               console.log('Reply sent');
+               await next();
+           });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
-}
+           this.onMembersAdded(async (context, next) => {
+               console.log('New member added to the conversation');
+               const membersAdded = context.activity.membersAdded;
+               const welcomeText = 'Hello and welcome!';
+               for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                   if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                       console.log(`Sending welcome message to member: ${membersAdded[cnt].id}`);
+                       await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                       console.log('Welcome message sent');
+                   }
+               }
+               await next();
+           });
 
-module.exports.EchoBot = EchoBot;
+           this.onTurn(async (context, next) => {
+               console.log(`Processing activity: ${context.activity.type}`);
+               await next();
+               console.log('Turn complete');
+           });
+       }
+   }
+
+   module.exports.EchoBot = EchoBot;
